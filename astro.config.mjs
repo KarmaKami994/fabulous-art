9aa0f768c09f0ab8e2ee143a15003c00495136de
Karim Moutiq <karim.moutiq@zh.ch>
2026-02-21 16:38:41 +0100

    added SEO mods

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.fabulous-art.ch',
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'de',
+        locales: {
+          de: 'de-CH',
+          en: 'en-US',
+        },
+      },
+      filter: (page) =>
+        // Exclude index redirect page
+        !page.endsWith('fabulous-art.ch/'),
+    }),
+  ],
   i18n: {
     defaultLocale: 'de',
     locales: ['de', 'en'],
